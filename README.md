cc1b1a79ba124f5bdb96b8d909e3a1e9961296fa
Chris Buttery <info@chrisbuttery.com>
2017-02-10 16:18:38 +1100

    0.18

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # github-user-repos
 
 Another [elm-lang](http://elm-lang.org/) demo using the github API.
-[ Try it out here ](http://chrisbuttery.github.io/elm-github-user-repos/)
 
-This demo app lists a GitHub user's repositories (30)
+This demo app lists a GitHub user's repositories (30).
+
+Upgraded to `0.18`.
 
 ![alt tag](https://github.com/chrisbuttery/elm-github-user-repos/blob/master/github-user-repos.gif)
 
--- src/Update.elm
+++ src/Update.elm
@@ -1,249 +1,259 @@
 module Update exposing (Msg(..), update)
 
 import Http
-import Json.Decode as Json exposing ((:=))
+import Json.Decode as Json exposing (field)
 import Task exposing (..)
 import Time exposing (Time)
 import String
-
 import Model exposing (Model, elapsedTimeOf, debounceTime, Repo)
+import Debug
 
 
 -- Msgs
 
+
 type Msg
-  = Input (Maybe String)
-  | SortBy String
-  | HideNotice
-  | FetchSuccess (Maybe (List Repo))
-  | FetchFail Http.Error
-  | Tick Time
-  | NoOp
+    = Input (Maybe String)
+    | SortBy String
+    | HideNotice
+    | FetchSuccess (Result Http.Error (Maybe (List Repo)))
+    | Tick Time
+    | NoOp
+
 
 
 -- update
 
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
+    case msg of
+        -- NoOp
+        -- return the current model
+        NoOp ->
+            ( model, Cmd.none )
 
-    -- NoOp
-    -- return the current model
+        -- Input
+        -- set the query value
+        -- If the query isn't 'Nothing'
+        -- call Tick with the current time.
+        Input query ->
+            case query of
+                Nothing ->
+                    ( { model
+                        | query = Nothing
+                        , notice = Nothing
+                      }
+                    , Cmd.none
+                    )
 
-    NoOp ->
-      ( model, Cmd.none)
+                Just str ->
+                    let
+                        model_ =
+                            { model
+                                | query = Just str
+                                , notice = Nothing
+                                , results = Nothing
+                            }
+                    in
+                        if str /= "" then
+                            ( model_, Task.perform Tick Time.now )
+                        else
+                            ( model_, Cmd.none )
 
+        -- SortBy
+        -- set the filter value
+        -- If the query isn't 'Nothing'
+        -- call Tick with the current time.
+        SortBy filter ->
+            case model.query of
+                Nothing ->
+                    ( { model | filter = filter }, Cmd.none )
 
-    -- Input
-    -- set the query value
-    -- If the query isn't 'Nothing'
-    -- call Tick with the current time.
+                Just query ->
+                    let
+                        model_ =
+                            { model
+                                | filter = filter
+                                , isLoading = True
+                            }
+                    in
+                        if query /= "" then
+                            ( model_, Task.perform Tick Time.now )
+                        else
+                            ( model_, Cmd.none )
 
-    Input query ->
-      case query of
-        Nothing ->
-          ( { model
-            | query = Nothing
-            , notice = Nothing
-            }, Cmd.none )
+        -- HideNotice
+        -- remove a notice on click
+        HideNotice ->
+            if model.notice == Nothing then
+                ( model, Cmd.none )
+            else
+                ( { model
+                    | notice = Nothing
+                  }
+                , Cmd.none
+                )
 
-        Just str ->
-          let
-            model' =
-              { model
-                | query = Just str
-                , notice = Nothing
+        -- FetchSuccess
+        -- on success of 'fetch' set the response on the model
+        -- if the reponse was an empty list,
+        -- define a notice.
+        -- set the isLoading state to False
+        FetchSuccess (Ok results) ->
+            case results of
+                Nothing ->
+                    ( { model
+                        | results = Nothing
+                        , isLoading = False
+                        , notice = Nothing
+                      }
+                    , Cmd.none
+                    )
+
+                Just repos ->
+                    if repos == [] then
+                        ( { model
+                            | results = Nothing
+                            , isLoading = False
+                            , notice = Just "No public repositories found for this user."
+                          }
+                        , Cmd.none
+                        )
+                    else
+                        ( { model
+                            | results = Just repos
+                            , isLoading = False
+                            , notice = Nothing
+                          }
+                        , Cmd.none
+                        )
+
+        -- FetchFail
+        -- on Http fail set :
+        -- the reponse as 'notice'
+        -- the isLoading state to False
+        -- result to Nothing
+        FetchSuccess (Err err) ->
+            ( { model
+                | notice = Just (httpErrorString err)
+                , isLoading = False
                 , results = Nothing
               }
-          in
-            if str /= ""
-            then
-              (model', Time.now |> Task.perform (\_ -> NoOp) Tick)
-            else
-              (model', Cmd.none)
+            , Cmd.none
+            )
+
+        -- Tick
+        -- using 'elapsedTimeOf' calulate a newElapsedTime
+        -- is 'newElapsedTime' the greather than 500 * millisecond ?
+        -- if so
+        -- set debounceState as Nothing and isLoading to True and perform
+        -- a Http request
+        -- if not don't make a Http request, but call Tick again
+        Tick clockTime ->
+            let
+                newElapsedTime =
+                    elapsedTimeOf model.debounceState clockTime
+            in
+                if newElapsedTime > debounceTime then
+                    ( { model
+                        | debounceState = Nothing
+                        , isLoading = False
+                      }
+                    , fetch (String.toLower (Maybe.withDefault "" model.query)) model.filter
+                    )
+                else
+                    ( { model
+                        | debounceState = Just { elapsedTime = newElapsedTime, prevClockTime = clockTime }
+                        , isLoading = True
+                      }
+                    , Task.perform Tick Time.now
+                    )
 
 
-    -- SortBy
-    -- set the filter value
-    -- If the query isn't 'Nothing'
-    -- call Tick with the current time.
 
-    SortBy filter ->
-      case model.query of
-        Nothing ->
-          ({ model | filter = filter }, Cmd.none)
-
-        Just query ->
-          let
-            model' =
-              { model
-                | filter = filter
-                , isLoading = True }
-          in
-            if query /= ""
-            then
-              (model', Time.now |> Task.perform (\_ -> NoOp) Tick)
-          else
-            (model', Cmd.none)
+-- httpErrorString
+-- Pattern match the error response.
+-- A MASSIVE thanks to this article:
+-- https://becoming-functional.com/http-error-checking-in-elm-fee8c4b68b7b#.6wz3kna56
 
 
-    -- HideNotice
-    -- remove a notice on click
+httpErrorString : Http.Error -> String
+httpErrorString error =
+    case error of
+        Http.BadUrl text ->
+            "Bad Url: " ++ text
 
-    HideNotice ->
-      if model.notice == Nothing
-      then (model, Cmd.none)
-      else
-        ({ model
-           | notice = Nothing
-        }, Cmd.none)
-
-
-    -- FetchSuccess
-    -- on success of 'fetch' set the response on the model
-    -- if the reponse was an empty list,
-    -- define a notice.
-    -- set the isLoading state to False
-
-    FetchSuccess results ->
-      case results of
-        Nothing ->
-          ({ model
-             | results = Nothing
-             , isLoading = False
-             , notice = Nothing
-            }, Cmd.none)
-
-        Just repos ->
-          if repos == []
-          then
-            ({ model
-               | results = Nothing
-               , isLoading = False
-               , notice = Just "No public repositories found for this user."
-              } , Cmd.none)
-          else
-            ({ model
-               | results = Just repos
-               , isLoading = False
-               , notice = Nothing
-              } , Cmd.none)
-
-
-    -- FetchFail
-    -- on Http fail set :
-    -- the reponse as 'notice'
-    -- the isLoading state to False
-    -- result to Nothing
-
-    FetchFail err ->
-      case err of
         Http.Timeout ->
-          ( { model
-              | notice = Just "Timeout"
-              , isLoading = False
-              , results = Nothing
-            }
-          , Cmd.none
-          )
+            "Http Timeout"
 
         Http.NetworkError ->
-          ( { model
-              | notice = Just "Network Error"
-              , isLoading = False
-              , results = Nothing
-            }
-          , Cmd.none
-          )
+            "Network Error"
 
-        Http.UnexpectedPayload error ->
-          ( { model
-              | notice = Just error
-              , isLoading = False
-              , results = Nothing
-            }
-          , Cmd.none
-          )
+        Http.BadStatus response ->
+            "Bad Http Status: " ++ toString response.status.code
 
-        Http.BadResponse code error ->
-          ( { model
-              | notice = Just error
-              , isLoading = False
-              , results = Nothing
-            }
-          , Cmd.none
-          )
+        Http.BadPayload message response ->
+            "Bad Http Payload: "
+                ++ toString message
+                ++ " ("
+                ++ toString response.status.code
+                ++ ")"
 
-
-    -- Tick
-    -- using 'elapsedTimeOf' calulate a newElapsedTime
-    -- is 'newElapsedTime' the greather than 500 * millisecond ?
-    -- if so
-    -- set debounceState as Nothing and isLoading to True and perform
-    -- a Http request
-    -- if not don't make a Http request, but call Tick again
-
-    Tick clockTime ->
-      let
-        newElapsedTime = elapsedTimeOf model.debounceState clockTime
-      in
-        if newElapsedTime > debounceTime
-        then
-          ({ model
-             | debounceState = Nothing
-             , isLoading = False
-           }
-           , fetch (String.toLower (Maybe.withDefault "" model.query)) model.filter
-          )
-        else
-          ({ model
-           | debounceState = Just { elapsedTime = newElapsedTime, prevClockTime = clockTime }
-           , isLoading = True
-           }
-           , Time.now |> Task.perform (\_ -> NoOp) Tick
-          )
 
 
 -- fetch
 -- take a query and a filter and perform a Http request
 
+
 fetch : String -> String -> Cmd Msg
 fetch query filter =
-  let url =
-    "https://api.github.com/users/" ++ query ++ "/repos?sort=" ++ filter
-  in
-    Task.perform FetchFail FetchSuccess (Http.get decodeRepos url)
+    let
+        url =
+            "https://api.github.com/users/" ++ query ++ "/repos?sort=" ++ filter
+
+        request =
+            Http.get url decodeRepos
+    in
+        Http.send FetchSuccess request
+
 
 
 -- decodeRepos
 -- return a decoded response we expect to receive
 
+
 decodeRepos : Json.Decoder (Maybe (List Repo))
 decodeRepos =
-  let repo =
-    Json.object7 Repo
-        (Json.maybe ("description" := Json.string))
-        ("html_url" := Json.string)
-        ("stargazers_count" := Json.int)
-        ("watchers_count" := Json.int)
-        ("full_name" := Json.string)
-        ("owner" := decodeAvatar)
-        ("owner" := decodeLogin)
-  in
-    Json.maybe <| Json.list repo
+    let
+        repo =
+            Json.map7 Repo
+                (Json.maybe (field "description" Json.string))
+                (field "html_url" Json.string)
+                (field "stargazers_count" Json.int)
+                (field "watchers_count" Json.int)
+                (field "full_name" Json.string)
+                (field "owner" decodeAvatar)
+                (field "owner" decodeLogin)
+    in
+        Json.maybe <| Json.list repo
+
 
 
 -- decodeAvatar
 -- pluck the avatar from 'owner' in the JSON response
 
+
 decodeAvatar : Json.Decoder String
 decodeAvatar =
-  Json.at ["avatar_url"] Json.string
+    Json.at [ "avatar_url" ] Json.string
+
 
 
 -- decodeLogin
 -- pluck the login from 'owner' in the JSON response
 
+
 decodeLogin : Json.Decoder String
 decodeLogin =
-  Json.at ["login"] Json.string
+    Json.at [ "login" ] Json.string
